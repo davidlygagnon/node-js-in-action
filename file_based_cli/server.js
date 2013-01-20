48e84ef217a54de7faa00eb3a176edce875aa54c
David <david.lygagnon@gmail.com>
2013-01-19 23:28:39 -0800

    update task cli

--- file_based_cli/server.js
+++ file_based_cli/server.js
@@ -6,7 +6,7 @@ var fs = require('fs'),
   file = path.join(process.cwd(), '/.tasks');
 
 switch (command) {
-  case: 'list':
+  case 'list':
     listTasks(file)
     break;
 
@@ -15,8 +15,44 @@ switch (command) {
     break;
 
   default:
-    console.log('Usage' : + process.argv[0]
+    console.log('Usage:'  + process.argv[0]
       + ' list|add [taskDescription]');
 }
 
-function
+function loadOrInitializeTaskArray(file, cb) {
+  fs.exists(file, function(exists) {
+    var tasks = [];
+    if (exists) {
+      fs.readFile(file, 'utf8', function (err, data) {
+        if (err) throw err;
+        var data = data.toString();
+        var tasks = JSON.parse(data);
+        cb(tasks);
+      });
+    } else {
+      cb([]);
+    }
+  });
+}
+
+function listTasks(file) {
+  loadOrInitializeTaskArray(file, function(tasks) {
+    for (var i in tasks) {
+      console.log(tasks[i]);
+    }
+  });
+}
+
+function storeTasks(file, tasks) {
+  fs.writeFile(file, JSON.stringify(tasks), 'utf8', function (err) {
+    if (err) throw err;
+    console.log('Saved.');
+  });
+}
+
+function addTask(file, taskDescription) {
+  loadOrInitializeTaskArray(file, function(tasks) {
+    tasks.push(taskDescription);
+    storeTasks(file, tasks);
+  });
+}
