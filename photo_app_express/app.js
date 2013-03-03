2868c68ac06aa921da62fe21bec55ea0c4ca9322
David <david.lygagnon@gmail.com>
2013-03-02 22:19:25 -0800

    adding upload photo feature of photo web app tutorial in node.js

--- photo_app_express/app.js
+++ photo_app_express/app.js
@@ -16,6 +16,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
+  app.set('photos', __dirname + '/public/photos');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -29,6 +30,8 @@ app.configure('development', function(){
 });
 
 app.get('/', photos.list);
+app.get('/upload', photos.form);
+app.post('/upload', photos.submit(app.get('photos')));
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- photo_app_express/routes/photos.js
+++ photo_app_express/routes/photos.js
@@ -16,3 +16,39 @@ exports.list = function (req, res) {
     photos : photos
   });
 };
+
+exports.form = function (req, res) {
+  res.render('photos/upload', {
+    title: 'Photo upload'
+  });
+};
+
+var Photo = require('../models/Photo');
+var path = require('path');
+var fs = require('fs');
+var join = path.join;
+
+exports.submit = function (dir) {
+  return function (req, res, next) {
+    var img = req.files.photo.image;
+    var name = req.body.photo.name || img.name;
+    var path = join(dir, img.name);
+
+    console.log("path is " + path);
+    console.log("img.path " + img.path);
+
+    fs.rename(img.path, path, function(err) {
+      if (err) {
+        console.log('error');
+        return next(err);
+      }
+      Photo.create({
+        name: name,
+        path: img.name
+      }, function (err) {
+        if (err) return next(err);
+        res.redirect('/');
+      });
+    });
+  }
+};
